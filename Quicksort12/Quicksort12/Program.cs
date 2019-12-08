ce9ead6eb6e1f66b9b352dd27557b24d73b7ad4c
Can Yaralı <mco2hy@gmail.com>
2019-12-08 18:39:26 +0300

    1'den 1000'e kadar sayıların kaç adet tek kaç adet çift olduğunu hesaplayalım. #12

---
+++ Quicksort12/Quicksort12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Quicksort12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac1 = 0;
+            int sayac2 = 0;
+            for (int i = 1; i < 1001; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sayac1++;
+                }
+                else
+                {
+                    sayac2++;
+                }
+            }
+            Console.WriteLine(sayac1 + " Çift");
+            Console.WriteLine(sayac2 + " Tek");
+            Console.ReadLine();
+        }
+    }
+}
