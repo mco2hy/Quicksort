c97a1e8df7ba659bbdca9dd9b5d9190c227c5d99
Can Yaralı <mco2hy@gmail.com>
2019-12-08 18:38:15 +0300

    Kullanıcının girdiği değer tek mi çift mi ? #11

---
+++ Quicksort11/Quicksort11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Quicksort11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Sayı Girin : ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                if (sayi % 2 == 0)
+                {
+                    Console.WriteLine("Çift");
+                }
+                else
+                {
+                    Console.WriteLine("Tek");
+                }
+            }
+        }
+    }
+}
+
