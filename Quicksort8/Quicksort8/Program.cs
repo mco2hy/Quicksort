3b89866aaaced33941943aec7e6edb78be261937
Can Yaralı <mco2hy@gmail.com>
2019-12-08 18:35:18 +0300

    İsim ve soyisminizi kullanıcının girdiği sayı kadar yazdırın. #8

---
+++ Quicksort8/Quicksort8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Quicksort8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı girin : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            string[] isim = { "Mehmet Can Onur YARALI" };
+            for (int i = 0; i < sayi; i++)
+            {
+                Console.WriteLine(isim[0]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
