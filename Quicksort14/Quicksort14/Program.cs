55c63ed6eabd72daf57bfebc10b573eba8c8592b
Can Yaralı <mco2hy@gmail.com>
2019-12-08 18:41:44 +0300

    Kullanıcıdan alınan kelimedeki karakter sayısını bulalım. #14

---
+++ Quicksort14/Quicksort14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Quicksort14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir kelime giriniz : ");
+            string kelime = Console.ReadLine();
+            int sayac = 0;
+
+            for (int i = 0; i <= kelime.Length-1; i++)
+            {
+                Console.WriteLine(kelime[i]);
+                sayac++;
+            }
+            Console.WriteLine(sayac);
+            Console.ReadLine();
+        }
+    }
+}
