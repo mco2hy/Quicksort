aa5b857844603599e270c62eabd143451c4816ea
Can Yaralı <mco2hy@gmail.com>
2019-12-08 17:58:07 +0300

    Tanımlı int arraydan en büyük elemanı bulalım #5

---
+++ Quicksort5/Quicksort5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Quicksort5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] can = { 1000, 900, 5000 };
+            int enBuyuk = 0;
+            
+            for (int i = 0; i <= can.Length - 1; i++)
+            {
+                if (can[i] > enBuyuk )
+                {
+                    enBuyuk = can[i];
+                }
+            }
+            Console.WriteLine(enBuyuk);
+            Console.ReadLine();
+        }
+    }
+}
