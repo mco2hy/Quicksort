be1d60d5c2fcb4fb55422b4f4992f5ffe143f103
Can Yaralı <mco2hy@gmail.com>
2019-12-08 17:56:59 +0300

    Tanımlı array sayılarını toplayalım #4

---
+++ Quicksort4/Quicksort4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Quicksort4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] can = { 1000, 900, 5 };
+            int toplam = 0;
+            for (int i = 0; i <= can.Length-1; i++)
+            {
+                toplam += can[i];
+            }
+            Console.WriteLine(toplam);
+            Console.ReadLine();
+        }
+    }
+}
