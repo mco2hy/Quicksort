9b1170da74439f62277990ba355916c827c2876d
Can Yaralı <mco2hy@gmail.com>
2019-12-08 18:43:07 +0300

    Girilen kelimede ki A-Z 0-9'a karakter sayılarını bulma. #15

---
+++ Quicksort15/Quicksort15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Quicksort15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "adfsfafsdfasdf";
+            List<char> f = test.ToList();
+            List<IGrouping<char, char>> grouped = f.GroupBy(a => a).OrderBy(a => a.Key).ToList();
+
+            foreach (IGrouping<char, char> item in grouped)
+            {
+                Console.WriteLine(item.Key + " : " + item.Count());
+            }
+            Console.ReadLine();
+        }
+    }
+}
