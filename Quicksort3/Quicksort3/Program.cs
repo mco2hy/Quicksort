0ca3361e7d1f55df927e0119a6c10457d9e54eb1
Can Yaralı <mco2hy@gmail.com>
2019-12-08 17:56:13 +0300

    0' dan 10' a kadar sayıları ekrana yazdıralım. #3

---
+++ Quicksort3/Quicksort3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Quicksort3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Sayı girin");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i <= sayi; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
