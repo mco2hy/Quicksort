bf5f3065bb5280e64917cd3ab3047cd3ab079f58
Can Yaralı <mco2hy@gmail.com>
2019-12-08 17:49:58 +0300

    Girilen kelimeyi tersini çevirip yazdırma. #1

---
+++ Quicksort1/Quicksort1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Quicksort1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Tersten Yazılacak Metni Giriniz = ");
+            string word = Console.ReadLine();
+            for (int i = 0; i <= word.Length - 1; i++)
+            {
+                Console.Write(word[word.Length-i-1]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
