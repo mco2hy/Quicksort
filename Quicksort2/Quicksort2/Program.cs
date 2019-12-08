220bec7aa82147a0f2ed3040c9f6be6cedcea8a1
Can Yaralı <mco2hy@gmail.com>
2019-12-08 17:55:27 +0300

    Palindrome #2

---
+++ Quicksort2/Quicksort2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Quicksort2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palindrome kontrolü için kelime giriniz : ");
+            string word = Console.ReadLine();
+
+            for (int i = 0; i <= word.Length - 1; i++)
+            {
+                if (word[word.Length - i - 1] == word[i])
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Palindrome değildir.");
+                    Console.ReadLine();
+                }
+            }
+            Console.WriteLine("Palindromedur.");
+            Console.ReadLine();
+        }
+    }
+}
