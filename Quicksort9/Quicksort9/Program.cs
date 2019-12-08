772752d10222c6e786da405deeee6078b7d85ff0
Can Yaralı <mco2hy@gmail.com>
2019-12-08 18:36:29 +0300

    Girilen 10 sayının negatif/pozitif olduğunu yazdırma. #9

---
+++ Quicksort9/Quicksort9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Quicksort9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] sayi = { -5, -4, 6, 10, -12, 0, 18, -20, 25, -26 };
+
+            for (int i = 0; i < sayi.Length - 1; i++)
+            {
+                if (sayi[i] < 0)
+                {
+                    Console.WriteLine(sayi[i] + " Negatif");
+                }
+                else
+                {
+                    Console.WriteLine(sayi[i] + " Pozitif");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
