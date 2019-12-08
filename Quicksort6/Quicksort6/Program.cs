d936dd5750465084423f65f835cce3fee1b2fdd2
Can Yaralı <mco2hy@gmail.com>
2019-12-08 17:58:53 +0300

    Tanımlı int arraydan en küçük elemanı bulalım #6

---
+++ Quicksort6/Quicksort6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Quicksort6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] can = { 10, 900, 25 };
+            int enKucuk = can[0];
+
+            for (int i = 0; i <= can.Length - 1; i++)
+            {
+                if (can[i] < enKucuk)
+                {
+                    enKucuk = can[i];
+                }
+            }
+            Console.WriteLine(enKucuk);
+            Console.ReadLine();
+        }
+    }
+    }
+
