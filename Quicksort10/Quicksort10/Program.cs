4fc15272485898dc014ddd54491122a30b980d9c
Can Yaralı <mco2hy@gmail.com>
2019-12-08 18:37:22 +0300

    Kullanıcıdan alınan değer int mi değil mi ? #10

---
+++ Quicksort10/Quicksort10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Quicksort10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+
+                Console.WriteLine("Sayı Girin : ");
+                try
+                {
+                    int sayi = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("İnteger");
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("İnteger değil");
+                }
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
