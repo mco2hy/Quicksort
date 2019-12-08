3d0fcf683c904dbb0a246d988e6adb57f5f58beb
Can Yaralı <mco2hy@gmail.com>
2019-12-08 18:34:23 +0300

    Tanımlı int array'deki 10 sayısının ortalamasına en yakın olanı bulalım. #7

---
+++ Quicksort7/Quicksort7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Quicksort7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double enKucuk = Double.MaxValue;
+            int yeri = 0;
+            List<double> fark = new List<double>();
+             
+             double [] sayi = { 5, 4, 6, 10, 12, 16, 18, 20, 25, 26 };
+
+
+            double ortalama = sayi.Average();
+
+            for (int i = 0; i <= sayi.Length - 1; i++)
+            {
+                fark.Add(Math.Abs(ortalama - sayi[i]));
+            }
+            for (int i = 0; i <= sayi.Length - 1; i++)
+            {
+                if (fark[i] < enKucuk)
+                {
+                    enKucuk = fark[i];
+                    yeri = i;
+                }
+
+            }
+            Console.WriteLine(fark[5]);
+            fark.ForEach(Console.WriteLine);
+            Console.WriteLine(ortalama);
+            Console.WriteLine(sayi[yeri]);
+
+
+            Console.ReadLine();
+        }
+    }
+}
