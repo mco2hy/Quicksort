a1208de913b6e7805b8b67e4fca7536b4068d93e
Mehmet Can Onur Yaralı <mco2hy@gmail.com>
2019-12-20 18:02:42 +0300

    BubbleSort #17

---
+++ Quicksort17_BubbleSort/Quicksort17_BubbleSort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Quicksort17_BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int swap;
+            int [] array = new int [] {24, 6, 19, 94, 1, 9, 0, 5};
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        swap = array[i];
+                        array[i] = array[j];
+                        array[j] = swap;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
