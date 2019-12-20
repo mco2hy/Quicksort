01a678c0574e24ae871edb295cb43d10ef6565e7
Mehmet Can Onur Yaralı <mco2hy@gmail.com>
2019-12-20 16:29:09 +0300

    Girilen cümlede ki kelime sayısını bulma. #16

---
+++ Quicksort16/Quicksort16/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Quicksort16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string metin;
+            Console.Write("Metni Girin  : ");
+            metin = Console.ReadLine();
+            string[] kelimeler = metin.Split(' ');
+            Console.WriteLine("Kelime Sayısı :" + kelimeler.Length);
+            foreach (string kelime in kelimeler)
+            {
+                Console.WriteLine(kelime);
+            }
+            Console.ReadKey();
+        }
+    }
+    }
+
